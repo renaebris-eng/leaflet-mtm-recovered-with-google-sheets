cc012e3fd5d758bbcbdd7e08fd49fc7ea2ad344d
renaebris-eng <renaebris@gmail.com>
2026-08-21 21:40:37 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Uh4khylvGQXegf22VDY-WvX7Y08Xbf9frta_xoXJfvI/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1t8fK4cDRL0MrhXikwuNAj5RlpW6hwKc6Mi4GrCaImHw/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBzVRsnkKxpCCJCo0MvzyhJyaTAJfgMMkk';
